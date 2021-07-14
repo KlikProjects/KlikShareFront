8ddfe1cd7b0aa2cd254f0951e827a20bafc2db76
solomonclark <you@example.com>
2021-07-14 09:52:33 +0200

    profile cambios de medidas

--- src/views/userProfile.vue
+++ src/views/userProfile.vue
@@ -1,43 +1,57 @@
 <template>
-    <div>
-        <button @click="goBack" type="button" class="exitbtn d-flex justify-content-start m-1">Volver</button>
-        <h4 class="mb-2">Estas viendo el perfil de <b>Sutanito</b></h4>
-            <h5 class="m-3"><b>En donación:</b></h5>
-            <div class="d-flex justify-content-start m-2">
-                <Card />
-                <Card />
+      
+   <div>
+      <div class="user  d-flex justify-content-between w-100 h-100 mt-">
+           <img class="usericon mt-3 m-1 " src="../assets/user.png" alt="usericon">
+          
+             <router-link to="/Gifts"><img class="usericonicon m-1 mt-3" src="../assets/coin.png" alt="usercoinsicon"> 
+            </router-link>
+       </div>
+        <div class="username d-flex justify-content-between">
+               <h4 class="username m-2 ">User Name</h4>
+               <h5 class="klikcoin m-2"><span >30 KiloCoins</span></h5>
+           </div>
+       
+        <div class="card ">
+            <div class="bnt d-flex justify-content-evenly mb-2">
+                <button class=" btn rounded-pill mt-3 ">Donado</button>
+                <button class=" btn rounded-pill mt-3">Regalado</button>
             </div>
-            <h5 class="m-3"><b>Donado:</b></h5>
-            <div class="d-flex justify-content-start">
-                <Card />
-                <Card />
+            <div class="products d-flex justify-content-evenly mb-2 ">
+            <img src="../assets/cuboard.png" alt="">
+            <img src="../assets/tomato.png" alt="">
             </div>
-            <h5 class="m-3"><b>Recibido:</b></h5>
-            <div class="d-flex justify-content-start">
-                <Card />
-                <Card />
-            </div>
+
+        </div>
     </div>
+
+ 
+ 
 </template>
+
 <script>
-import Card from "@/components/Card.vue";
+
 export default {
-  name: "card",
-  components: {
-    Card,
-  },
-  methods: {
-    goBack() {
-      this.$router.push("/infoCard");
-    },
-  },
-};
-</script>
-<style scoped>
-.exitbtn {
-  background-color: #4a483f;
-  color: #a4ebf3;
-  border-radius: 50%;
-  margin-top: 20%;
+  name: 'UserProfile',
+  props: {
+  }
 }
+</script>
+<style>
+.btn{
+ background-color:#2e2603;
+  color: #A4EBF3;
+
+}
+span{
+  color:green;
+}
+.span:hover .img {
+  transform: translateY(0) scale(2.2);
+}
+.username:hover{
+  color:green;
+  cursor: pointer;
+}
+
 </style>
